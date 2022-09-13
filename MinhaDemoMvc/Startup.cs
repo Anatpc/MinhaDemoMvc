b4f2d4fa2f26f155bb794071ebd3bbb80b8c6bc9
Anatpc <ana.tpc2022@gmail.com>
2022-09-12 21:21:11 -0300

    Scaffold

--- MinhaDemoMvc/Startup.cs
+++ MinhaDemoMvc/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinhaDemoMvc.Data;
 
 namespace MinhaDemoMvc
 {
@@ -24,6 +26,9 @@ namespace MinhaDemoMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MinhaDemoMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MinhaDemoMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
